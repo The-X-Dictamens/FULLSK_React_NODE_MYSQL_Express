5dcbc0bac5e0335fe281e4d8400c8d111fd0f9d3
Alexxomby <72172506+Alexxomby@users.noreply.github.com>
2024-03-13 00:21:34 -0600

    Juan mi Patron
    al juan se le reza o se le besa

--- reactfront/src/blog/EditBlog.js
+++ reactfront/src/blog/EditBlog.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-
+//se quito el gitignore
 const URI = 'http://localhost:8000/blogs/'
 
 const CompEditBlog = () => {
